28c46f7fadc564d20e0410e1f5fabd2ac41696c4
erhan sakarya <erhansakaryaa@gmail.com>
2023-10-10 15:31:34 +0300

    tested wifi ap mode.

---
+++ wifi-ap-mode/firmware/sample_project/components/wifi_connect/wifi_connect.h
@@ -0,0 +1,8 @@
+#ifndef WIFI_CONNECT_H
+#define WIFI_CONNECT_H
+
+void wifi_connect_init(void);
+esp_err_t wifi_connect_sta(char * ssid, char * password, int timeout);
+void wifi_connect_ap(const char * ssid, const char * password);
+
+#endif
